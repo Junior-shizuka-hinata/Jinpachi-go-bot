999d66c99cefba45ae4be0044f2c3a238d8d7fda
Shi-zu-ka-27 <jeanjuniors210@gmail.com>
2024-09-18 16:42:40 +0100

    Update hinata.js

--- scripts/cmds/hinata.js
+++ scripts/cmds/hinata.js
@@ -28,7 +28,7 @@ async function getAIResponse(input, userId, messageID) {
         { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
     ];
 
-    let response = "𝗕𝗼𝗻𝗷𝗼𝘂𝗿! 𝗝𝗲 𝘀𝘂𝗶𝘀 𝗹à 𝗽𝗼𝘂𝗿 𝘁'𝗮𝗶𝗱𝗲𝗿 𝗲𝘁 𝗿é𝗽𝗼𝗻𝗱𝗿𝗲 à 𝘁𝗲𝘀 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀. 𝗡'𝗵é𝘀𝗶𝘁𝗲 𝗽𝗮𝘀 à 𝗺𝗲 𝗱𝗲𝗺𝗮𝗻𝗱𝗲𝗿 𝗰𝗲 𝗾𝘂𝗲 𝘁𝘂 𝘃𝗲𝘂𝘅!....🍀";
+    let response = "𝗕𝗼𝗻𝗷𝗼𝘂𝗿! 𝗕𝗲𝘀𝗼𝗶𝗻 𝗱'𝗮𝗶𝗱𝗲....?🍀";
     let currentIndex = 0;
 
     for (let i = 0; i < services.length; i++) {
@@ -60,12 +60,12 @@ module.exports = {
     onStart: async function ({ api, event, args }) {
         const input = args.join(' ').trim();
         if (!input) {
-            api.sendMessage(`🫰✰`, event.threadID, event.messageID);
+            api.sendMessage(`☆.....☆`, event.threadID, event.messageID);
             return;
         }
 
         const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
-        api.sendMessage(`✰...𝐇𝐢𝐧𝐚𝐭𝐚 𝐩𝐫𝐨𝐜𝐞̀𝐝𝐞 𝐚 𝐯𝐨𝐭𝐫𝐞 𝐫𝐞𝐪𝐮𝐞̂𝐭𝐞...✰`, event.threadID, messageID);
+        api.sendMessage(`✰...𝐬𝐡𝐢𝐳𝐮𝐤𝐚 𝐩𝐫𝐨𝐜𝐞̀𝐝𝐞 𝐚 𝐯𝐨𝐭𝐫𝐞 𝐫𝐞𝐪𝐮𝐞̂𝐭𝐞...✰`, event.threadID, messageID);
     },
     onChat: async function ({ event, message }) {
         const messageContent = event.body.trim().toLowerCase();
@@ -73,7 +73,7 @@ module.exports = {
             const input = messageContent.replace(/^hinata\s*/, "").trim();
             const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
             // Construct message with special fonts
-            const formattedResponse = ` ✿❯─-───💚───-─❮✿\n𝐉'𝐞𝐬𝐩𝐞̀𝐫𝐞 𝐪𝐮𝐞 𝐜𝐞𝐭𝐭𝐞 𝐫𝐞́𝐩𝐨𝐧𝐬𝐞\n 𝐭𝐞 𝐬𝐚𝐭𝐢𝐬𝐟𝐚𝐢𝐭\n✿─-───💚───-─✿\n✿─❮${response}💚❯─✿\n✿─-───💚───-─✿\n𝐌𝐞𝐫𝐜𝐢 𝐝'𝐚𝐯𝐨𝐢𝐫 𝐩𝐚𝐭𝐢𝐞𝐧𝐭𝐞́\n✿❯─-───💚───-─❮✿`;
+            const formattedResponse = ` ✿❯─-───🌺───-─❮✿\n𝐉'𝐞𝐬𝐩𝐞̀𝐫𝐞 𝐪𝐮𝐞 𝐜𝐞𝐭𝐭𝐞 𝐫𝐞́𝐩𝐨𝐧𝐬𝐞\n 𝐭𝐞 𝐬𝐚𝐭𝐢𝐬𝐟𝐚𝐢𝐭\n✿─-───🌺───-─✿\n✿─❮${response}🌺❯─✿\n✿─-───🌺───-─✿\n𝐌𝐞𝐫𝐜𝐢 𝐝'𝐚𝐯𝐨𝐢𝐫 𝐩𝐚𝐭𝐢𝐞𝐧𝐭𝐞́\n✿❯─-───🌺───-─❮✿`;
             message.reply(formattedResponse, messageID);
         }
     }
